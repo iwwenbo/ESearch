029c936e6f17bb0fd51f405f7dcd689e5160738a
iwwenbo <iwwenbo@gmail.com>
2016-08-02 19:59:26 +0800

    search

--- src/com/es/action/RxSearchAction.java
+++ src/com/es/action/RxSearchAction.java
@@ -1,19 +1,37 @@
 package com.es.action;
 
 import com.es.service.IRxSearchService;
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by iwwenbo on 2016/7/27.
  */
-public class RxSearchAction {
+
+@Controller
+@Scope("prototype")
+public class RxSearchAction extends ActionSupport {
+    //搜索关键字
+    private String keyWords;
 
     private IRxSearchService rxSearchService;
 
     /**
-     * 进行搜索的方法
+     * 跳转到搜索页面
      */
-    private void doSearchPage(){
+    public  String doSearchPage(){
         System.out.println("正在进行搜索。。。");
+        return "success";
+    }
+
+    /**
+     * 根据关键字进行搜索
+     * @return
+     */
+    public String doSearch(){
+        System.out.println(keyWords+"----------搜索内容");
+        return "success";
     }
 
     public IRxSearchService getRxSearchService() {
@@ -22,5 +40,13 @@ public class RxSearchAction {
 
     public void setRxSearchService(IRxSearchService rxSearchService) {
         this.rxSearchService = rxSearchService;
+    }
+
+    public String getKeyWords() {
+        return keyWords;
+    }
+
+    public void setKeyWords(String keyWords) {
+        this.keyWords = keyWords;
     }
 }
